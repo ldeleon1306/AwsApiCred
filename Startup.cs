2f5f8b1f1f120aa88ad3729f9496ca04d6df901b
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-04-05 23:18:55 -0300

    inicial

--- Startup.cs
+++ Startup.cs
@@ -33,8 +33,8 @@ namespace AwsApiCred
             services.AddControllersWithViews();
             services.AddScoped<ContratosController>();
             services.AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo(@"c:\temp\keys"))
-    .ProtectKeysWithCertificate("thumbprint");
+    .PersistKeysToFileSystem(new DirectoryInfo(@"c:\temp\keys"));
+    //.ProtectKeysWithCertificate("thumbprint");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
